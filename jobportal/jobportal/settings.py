6505908bbf5397b176790246d95cf38fd65b342d
AntonOstap76 <antonostapchuk76@gmail.com>
2025-01-24 08:37:10 +0100

    test

--- jobportal/jobportal/settings.py
+++ jobportal/jobportal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$2p1+4dcz2^-(#t72be0(=nf+!n^&*b0^pgmacya*z4z3-77$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'False'
+DEBUG = 'True'
 
-ALLOWED_HOSTS = ['steve-jobs-cgc3gudkhsfth9an.polandcentral-01.azurewebsites.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -130,9 +130,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-    BASE_DIR/'static'
+    BASE_DIR/'staticfiles'
 ]
-STATICFILES_STORAGE='whitenoise.storage.CompressedMainfestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- jobportal/jobportal/wsgi.py
+++ jobportal/jobportal/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for jobportal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobportal.settings')
+settings_module = 'jobportal.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'jobportal.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
